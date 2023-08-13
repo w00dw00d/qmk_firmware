e17435c2225df92e360d29dd856b7d072cfdc0b2
w__d <w__d@outlook.com>
2023-08-13 17:49:07 +0900

    little fix

--- keyboards/osoba/osokbd/keymaps/default/keymap.c
+++ keyboards/osoba/osokbd/keymaps/default/keymap.c
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_split_3x6_4(
         _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,    KC_F9,   KC_F10,   KC_F11,   KC_F12,    KC_GRV,
         KC_CAPS,  _______, KC_QUOT, _______, _______, _______, _______, _______, KC_UP,    _______, _______, _______,   KC_BSLS,
-        _______,  _______, _______, _______, _______, _______, KC_LNG2, KC_LEFT, KC_DOWN,  KC_RGHT, _______, _______,
+        _______,  _______, _______, _______, _______, _______, KC_LNG2, KC_LEFT, KC_DOWN,  KC_RGHT, KC_MINUS, _______,
         _______,  _______, _______, _______, _______, _______, KC_LNG1, _______, _______,  _______, _______, _______,
-        XXXXXXX,  XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______, _______
+        XXXXXXX,  XXXXXXX, XXXXXXX, _______, _______, _______, KC_DEL, _______, _______
     )
 };
 
